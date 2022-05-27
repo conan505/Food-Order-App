311074ebb961c881b0cb288c128136050448dc28
Rahul Garg <thisisrg.iitr@gmail.com>
2022-05-28 01:17:18 +0530

    add meals items form component and global input component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css"
+import MealItemForm from "./MealItemForm"
 
 const MealItem = (props) => {
     return <li className={classes.meal}>
@@ -12,9 +13,7 @@ const MealItem = (props) => {
             </div>
 
         </div>
-        <div>
-
-        </div>
+        <MealItemForm />
     </li>
 }
 
