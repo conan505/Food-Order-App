027a5772549f6bf89a96267277fc9b6425d3cb23
Rahul Garg <thisisrg.iitr@gmail.com>
2022-06-03 23:42:10 +0530

    add animation on cart icon when we add new items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,34 @@
 import { useContext } from "react"
+import { useEffect, useState } from "react"
 import CartContext from "../../store/cart-context"
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = (props) => {
+    const [isCartHighlighted, setIsCartHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
     const cartItems = cartCtx.items.reduce((currentValue, item) => {
         return currentValue + item.amount;
     }, 0)
-    return <button onClick={props.onClick} className={classes.button}>
+
+    const btnclass = `${classes.button} ${isCartHighlighted ? classes.bump : ''}`
+
+    useEffect(() => {
+        if (cartCtx.items.length !== 0) {
+            setIsCartHighlighted(true);
+        }
+
+        const timer = setInterval(() => { setIsCartHighlighted(false) }, 300);
+
+        return (() => {
+            clearInterval(timer);
+        })
+
+
+    }, [cartCtx.items])
+
+    return <button onClick={props.onClick} className={btnclass}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
